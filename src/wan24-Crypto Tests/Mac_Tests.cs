fb0761ee576b793d8a4212268efdd2a06de94d2b
nd <nd1012@users.noreply.github.com>
2024-01-12 13:56:25 +0700

    Update Mac_Tests.cs

--- src/wan24-Crypto Tests/Mac_Tests.cs
+++ src/wan24-Crypto Tests/Mac_Tests.cs
@@ -12,7 +12,7 @@ namespace wan24_Crypto_Tests
         [TestMethod]
         public void MacHelper_Tests()
         {
-            Assert.AreEqual(MacHmacSha512Algorithm.ALGORITHM_NAME, MacHelper.DefaultAlgorithm.Name);
+            Assert.AreEqual(MacHmacSha3_512Algorithm.ALGORITHM_NAME, MacHelper.DefaultAlgorithm.Name);
             Assert.AreEqual(MacHelper.DefaultAlgorithm.MacLength, TestData.Data.Hash().Length);
             Assert.AreEqual(MacHelper.DefaultAlgorithm, MacHelper.GetAlgorithm(MacHelper.DefaultAlgorithm.Name));
             Assert.AreEqual(MacHelper.DefaultAlgorithm, MacHelper.GetAlgorithm(MacHelper.DefaultAlgorithm.Value));
