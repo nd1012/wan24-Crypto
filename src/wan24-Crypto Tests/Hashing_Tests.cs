c99635d27f95a82d2146dc4e5b2391d25af4f34f
nd <nd1012@users.noreply.github.com>
2024-01-12 13:59:09 +0700

    Update Hashing_Tests.cs

--- src/wan24-Crypto Tests/Hashing_Tests.cs
+++ src/wan24-Crypto Tests/Hashing_Tests.cs
@@ -12,7 +12,7 @@ namespace wan24_Crypto_Tests
         [TestMethod]
         public void HashHelper_Tests()
         {
-            Assert.AreEqual(HashSha512Algorithm.ALGORITHM_NAME, HashHelper.DefaultAlgorithm.Name);
+            Assert.AreEqual(HashSha3_512Algorithm.ALGORITHM_NAME, HashHelper.DefaultAlgorithm.Name);
             Assert.AreEqual(HashHelper.DefaultAlgorithm.HashLength, TestData.Data.Hash().Length);
             Assert.AreEqual(HashHelper.DefaultAlgorithm, HashHelper.GetAlgorithm(HashHelper.DefaultAlgorithm.Name));
             Assert.AreEqual(HashHelper.DefaultAlgorithm, HashHelper.GetAlgorithm(HashHelper.DefaultAlgorithm.Value));
