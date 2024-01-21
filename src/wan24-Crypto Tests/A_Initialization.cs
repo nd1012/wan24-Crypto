1b67f7e3eeb78d338595dc027770444515fed348
nd <nd1012@users.noreply.github.com>
2024-01-21 07:55:39 +0100

    Update
    + Added `SharedTests.Initialize`

--- src/wan24-Crypto Tests/A_Initialization.cs
+++ src/wan24-Crypto Tests/A_Initialization.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
-using System.Security.Cryptography;
 using wan24.Core;
-using wan24.Crypto;
+using wan24.Crypto.Tests;
 using wan24.ObjectValidation;
 
 namespace wan24_Crypto_Tests
@@ -25,31 +24,8 @@ namespace wan24_Crypto_Tests
             {
                 if(info.Exception is StackInfoException six) Logging.WriteError(six.StackInfo.Stack);
             };
+            SharedTests.Initialize();
             ValidateObject.Logger("wan24-Crypto Tests initialized");
-            // Disable algorithms which are not supported in this platform
-            if (!Shake128.IsSupported)
-            {
-                // SHA3 hash
-                HashHelper.DefaultAlgorithm = HashSha512Algorithm.Instance;
-                HashHelper.Algorithms.TryRemove(HashSha3_256Algorithm.ALGORITHM_NAME, out _);
-                HashHelper.Algorithms.TryRemove(HashSha3_384Algorithm.ALGORITHM_NAME, out _);
-                HashHelper.Algorithms.TryRemove(HashSha3_512Algorithm.ALGORITHM_NAME, out _);
-                HashHelper.Algorithms.TryRemove(HashShake128Algorithm.ALGORITHM_NAME, out _);
-                HashHelper.Algorithms.TryRemove(HashShake256Algorithm.ALGORITHM_NAME, out _);
-                // SHA3 HMAC
-                MacHelper.DefaultAlgorithm = MacHmacSha512Algorithm.Instance;
-                MacHelper.Algorithms.TryRemove(MacHmacSha3_256Algorithm.ALGORITHM_NAME, out _);
-                MacHelper.Algorithms.TryRemove(MacHmacSha3_384Algorithm.ALGORITHM_NAME, out _);
-                MacHelper.Algorithms.TryRemove(MacHmacSha3_512Algorithm.ALGORITHM_NAME, out _);
-                // Pake default options
-                Pake.DefaultOptions = Pake.DefaultOptions
-                    .WithMac(MacHmacSha512Algorithm.ALGORITHM_NAME, included: false);
-                Pake.DefaultCryptoOptions = Pake.DefaultCryptoOptions
-                    .WithMac(MacHmacSha512Algorithm.ALGORITHM_NAME, included: false);
-                // KDF (don't use SHA3 and remove SP800-108)
-                KdfHelper.Algorithms.TryRemove(KdfSp800_108HmacCtrKbKdfAlgorithm.ALGORITHM_NAME, out _);
-                KdfPbKdf2Options.DefaultHashAlgorithm = HashSha384Algorithm.ALGORITHM_NAME;
-            }
         }
     }
 }
